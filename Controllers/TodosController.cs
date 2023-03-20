8aab945db43037addb13318398a2863adb58de2e
Robert Cato <saiwolf@swmnu.net>
2023-03-20 09:29:17 -0500

    Added Todos Controller

---
+++ Controllers/TodosController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoListSPA.Controllers;
+
+[Route("api/todos")]
+[ApiController]
+public class TodosController : ControllerBase
+{
+}
